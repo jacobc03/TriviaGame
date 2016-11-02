22849e6aed05467c7c65f2efc20d36a6d20a1c53
Jacob <jacob@Jacobs-MacBook-Air.local>
2016-11-01 21:58:16 -0400

    jquery update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+
+//Questions with choices and which index holds the correct answer
 var allQuestions = [{
     question: 'What is the last name of a bastard child born of the North?',
     choices: ['Sand', 'Storm', 'Snow', 'Flowers'],
@@ -38,25 +40,91 @@ var allQuestions = [{
     choices: ['Rickon', 'Arya Stark', 'Bran Stark', 'Robb Stark'],
     correctAnswer: 4
 }];
+//Game Starts when the start button is clicked
+$('#startButton').click(function(){
+  $('#startButton').hide();
 
-//$('#startButton').click(function(){
+    var currentQuestion = 0;
+    var rightAnswers=0;
+    var wrongAnswers=0;
+    var div1= $("<div>");
+    var div2= $('<div>');
+    var div3= $('<div>');
+    var div4= $('<div>');
+    var div5= $('<div>');
 
-//})
-var x = 0;
-var div1= $("<div>").html(allQuestions[x].question);
-var div2= $('<div>').html(allQuestions[x].choices[0]);
-var div3= $('<div>').html(allQuestions[x].choices[1]);
-var div4= $('<div>').html(allQuestions[x].choices[2]);
-var div5= $('<div>').html(allQuestions[x].choices[3]);
-div1.attr('class',"questions");
-div2.attr('class',"answers");
-div3.attr('class',"answers");
-div4.attr('class',"answers");
-div5.attr('class',"answers");
-$('#gameArea').append(div1).append(div2).append(div3).append(div4).append(div5);
+    function gameAreaUpdate(){
+        div1.html(allQuestions[currentQuestion].question);
+        div2.html(allQuestions[currentQuestion].choices[0]);
+        div3.html(allQuestions[currentQuestion].choices[1]);
+        div4.html(allQuestions[currentQuestion].choices[2]);
+        div5.html(allQuestions[currentQuestion].choices[3]);
+        div1.attr('class',"questions");
+        div2.attr('class',"answers ahover");
+        div3.attr('class',"answers ahover");
+        div4.attr('class',"answers ahover");
+        div5.attr('class',"answers ahover");
+    }
+//call gameAreaUpdate fuction when start button is clicked.
+    gameAreaUpdate();
+    $('#gameArea').append(div1).append(div2).append(div3).append(div4).append(div5);
+ 
+    
+    var time= 10;
+    $("#timeRemain").html(time);
+    var counter; 
 
-var time= 30;
-$("#timeRemain").html(time);
+    function startTimer(){
+        counter=setInterval(timer, 1000);//1000 will  run it every 1 second
+    }
+//Start timer countdown when sart button is clicked
+    startTimer();
 
+    function timer(){
+        time--;
+        $("#timeRemain").html(time);
+        if (time <= 0){
+         clearInterval(counter);
+         //counter ended, do something here
+         $("#timeRemain").html(time);
+         currentQuestion++;
+         gameAreaUpdate();
+         time=10;
+         $("#timeRemain").html(time);
+        startTimer();
+         
+
+      } 
+     }
+
+$('div.answers').click(function(){
+    var userPick=$(this).html();
+    if (userPick==allQuestions[currentQuestion].choices[correctAnswer]) {
+        rightAnswers++;
+        time=10;
+        $("#timeRemain").html(time);
+        clearInterval(counter);
+        startTimer();
+        currentQuestion++;
+        gameAreaUpdate();
+        console.log("right:" + rightAnswers);
+    }else{
+        wrongAnswers++;
+        time=10;
+    $("#timeRemain").html(time);
+    clearInterval(counter);
+    startTimer();
+    currentQuestion++;
+    gameAreaUpdate();
+    console.log("wrong"+wrongAnswers);
+    }
+    
+
+});
+
+
+
+
+    })
 
 
